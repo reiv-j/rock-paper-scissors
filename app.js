b15148bcfac8a2abe19f8e31d309343935cdae4b
reiv <javierromaynieto@gmail.com>
2024-01-19 13:49:04 +0100

    Create playRound function

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 function getComputerChoice() {
     const rand = Math.floor(Math.random() * 3);
     if (rand === 0) {
-        return 'Rock';
+        return 'rock';
     } else if (rand === 1) {
-        return 'Paper';
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    console.log("Player selection: ", playerSelection);
+    console.log("Computer selection: ", computerSelection);
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else {
+        if (playerSelection === 'rock') {
+            if (computerSelection === 'scissors') {
+                return "Player wins!";
+            } else {
+                return "Computer wins!";
+            }
+        } else if (playerSelection === "paper") {
+            if (computerSelection === "rock") {
+                return "Player wins!";
+            } else {
+                return "Computer wins!";
+            }
+        } else {
+            if (computerSelection === "paper") {
+                return "Player wins!";
+            } else {
+                return "Computer wins!"
+            }
+        }
+    }
+}
+
+
